ba705595bb6f5989be7d0a07dbdd4115438a64b0
saakshi2701 <100992372+saakshi2701@users.noreply.github.com>
2025-12-07 20:30:04 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// Name of the cache
+const CACHE_NAME = "egg-timer-cache-v1";
+
+// Files to cache
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./egg.svg"
+];
+
+// Install event
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
