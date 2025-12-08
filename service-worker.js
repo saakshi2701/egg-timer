6be6e924d2a51b899d0d8b309c4642555ce52c7f
saakshi2701 <100992372+saakshi2701@users.noreply.github.com>
2025-12-08 19:29:43 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -26,3 +26,12 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
+
